c8f5b2048cc51eb30e292f4e6ff323db33366ca5
Steve Lacy <me@slacy.me>
2014-11-20 14:06:29 -0700

    update depreciated

--- server/http/express/spa.js
+++ server/http/express/spa.js
@@ -3,7 +3,7 @@ var config = require('../../config');
 var join = require('path').join;
 var idxFile = join(config.pubdir, 'index.html');
 app.get('/*', function(req, res) {
-  return res.sendfile(idxFile);
+  return res.sendFile(idxFile);
 });
 
 module.exports = app;
